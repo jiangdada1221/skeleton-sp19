fdb3a8034b00de654b224dd064413ac6d66812a5
yuepengjiang <jiangdada12344321@163.com>
2019-05-25 19:42:54 +0800

    Body

--- proj0/Body.java
+++ proj0/Body.java
@@ -1,11 +1,11 @@
 public class Body{
-  static double G = 6.67e-11;
-  double xxPos;
-  double yyPos;
-  double xxVel;
-  double yyVel;
-  double mass;
-  String imgFileName;
+  public static double G = 6.67e-11;
+  public double xxPos;
+  public double yyPos;
+  public double xxVel;
+  public double yyVel;
+  public double mass;
+  public String imgFileName;
   public Body(double xP, double yP, double xV, double yV,double m, String img){
     xxPos = xP;
     yyPos = yP;
